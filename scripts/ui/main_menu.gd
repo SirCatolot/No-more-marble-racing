fdc5db986bf318b73e693153b9798a531023dd16
Landon Martin <lmm1409@yahoo.com>
2025-10-23 00:13:45 -0400

    fixed rooting with game.
    starts on main menu now. if user presses play, will go to the game

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -5,7 +5,7 @@ func _ready():
 	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
 	
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/game/GameRoot.tscn")
+	get_tree().change_scene_to_file("res://scenes/game/main.tscn")
 	print("Play Pressed")
 
 func _on_quit_pressed():
