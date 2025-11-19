0b1ea7618d019a15591a02a56b9546e75febec35
Landon Martin <lmm1409@yahoo.com>
2025-11-19 16:20:36 -0500

    fixed naming schemes
    fixed naming schemes and delted tiles folder as it was useless

--- singletons/path_spawner_stage_3.gd
+++ singletons/path_spawner_stage_3.gd
@@ -4,9 +4,9 @@ signal round_started(round)
 signal round_finished(round)
 
 # Preload different enemy path scenes
-@onready var MarbleA = preload("res://Assets/marbles/Stage 3 Marble A.tscn")
-@onready var MarbleB = preload("res://Assets/marbles/Stage 3 Marble B.tscn")
-@onready var MarbleC = preload("res://Assets/marbles/Stage 3 Marble C.tscn")
+@onready var MarbleA = preload("res://assets/marbles/Stage3MarbleA.tscn")
+@onready var MarbleB = preload("res://assets/marbles/Stage3MarbleB.tscn")
+@onready var MarbleC = preload("res://assets/marbles/Stage3MarbleC.tscn")
 
 # Track current round and how many enemies have spawned
 var currentRound = 1
