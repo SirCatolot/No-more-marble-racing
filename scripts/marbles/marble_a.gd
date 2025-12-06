457bf5908dadf0dbc0b5e71717ebd1f53c37527e
isaac <Isaacformato@gmail.com>
2025-12-05 23:48:10 -0500

    adding marbles to rounds in desert map #43

--- scripts/marbles/marble_a.gd
+++ scripts/marbles/marble_a.gd
@@ -10,10 +10,20 @@ func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	
 	# Despawns the enemy and removes a life from player if they reach the end of the path
-	if get_parent().get_progress_ratio() == 1:
+	if get_parent().get_progress_ratio() >= 1:
 		GameState.lose_life(1)
-		queue_free()
+		_cleanup()
 	# If enemies health reaches zero, the player gains money and the enemy is deleted.
 	if Health <= 0:
 		GameState.add_money(5)
-		get_parent().get_parent().queue_free()
+		_cleanup()
+
+func _cleanup():
+	var parent = get_parent()
+	var grandparent = parent.get_parent()
+	# In Desert Level, we only want to delete the PathFollow2D (parent)
+	# In Stage 1, we want to delete the whole Path2D container (grandparent)
+	if grandparent.name == "LevelPath":
+		parent.queue_free()
+	else:
+		grandparent.queue_free()
---
+++ singletons/path_spawner_desert.gd
@@ -0,0 +1,88 @@
+extends Node2D
+
+signal round_started(round)
+signal round_finished(round)
+
+# Preload different enemy path scenes
+@onready var MarbleA = preload("res://scenes/marbles/MarbleA.tscn")
+@onready var MarbleB = preload("res://scenes/marbles/MarbleB.tscn")
+@onready var MarbleC = preload("res://scenes/marbles/MarbleC.tscn")
+
+# Track current round and how many enemies have spawned
+var currentRound = 1
+var enemiesSpawned = 0
+var enemiesPerRound = 15  # Starts harder than Stage 1 (was 10)
+var is_round_active = false
+var has_started = false
+
+@onready var level_path = get_parent().get_node("LevelPath")
+
+func _ready():
+	add_to_group("path_spawner")
+	# Shows the initial round value in UI
+	GameState.set_round(currentRound)
+	$Timer.stop()
+
+## Spawns a new enemy path instance each time the timer triggers
+func _on_timer_timeout():
+	var marble_instance
+	var path_follow = PathFollow2D.new()
+	path_follow.loop = false
+	path_follow.rotates = true
+
+	# Pick which enemy type to spawn based on the round
+	if currentRound == 1:
+		marble_instance = MarbleA.instantiate()
+	elif currentRound == 2:
+		marble_instance = MarbleB.instantiate()
+	elif currentRound == 3:
+		marble_instance = MarbleC.instantiate()
+	else:
+		# After round 3, mix them randomly with higher chance for harder marbles
+		var r = randf()
+		if r < 0.3:
+			marble_instance = MarbleA.instantiate()
+		elif r < 0.6:
+			marble_instance = MarbleB.instantiate()
+		else:
+			marble_instance = MarbleC.instantiate()
+
+	path_follow.add_child(marble_instance)
+	level_path.add_child(path_follow)
+	enemiesSpawned += 1
+
+	# Round 1: No slow down for Desert, keep the pressure up
+	
+	# When enough enemies have spawned allow starting next round
+	if enemiesSpawned >= enemiesPerRound:
+		$Timer.stop()
+		is_round_active = false
+		GameState.add_money(150 + (currentRound * 15)) # Higher bonus for harder map
+		emit_signal("round_finished", currentRound)
+	
+func start_next_round():
+	# Guard against double start
+	if is_round_active:
+		return
+
+	# First time starting: do not increment round
+	if not has_started:
+		has_started = true
+		enemiesSpawned = 0
+		is_round_active = true
+		emit_signal("round_started", currentRound)
+		# Fast start
+		$Timer.start(0.8) 
+		return
+
+	# Subsequent rounds: advance and increase difficulty
+	currentRound += 1
+	GameState.set_round(currentRound)
+	enemiesSpawned = 0
+	enemiesPerRound += 4 # Scale up faster (was +2)
+	is_round_active = true
+	emit_signal("round_started", currentRound)
+	
+	# Decrease spawn time as rounds progress to increase intensity
+	var spawn_time = max(0.2, 0.8 - (currentRound * 0.1))
+	$Timer.start(spawn_time)
